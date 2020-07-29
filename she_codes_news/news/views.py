02b31af643dfe363ac5a67996d88313c61c2ed4a
Vivianne <viviannevilar@gmail.com>
2020-07-29 20:30:25 +0800

    creating a news story

--- she_codes_news/news/views.py
+++ she_codes_news/news/views.py
@@ -1,6 +1,13 @@
 from django.views import generic
+from django.urls import reverse_lazy
 from .models import NewsStory
+from .forms import StoryForm
 
+class AddStoryView(generic.CreateView):
+    form_class = StoryForm
+    context_object_name = 'storyForm'
+    template_name = 'news/createStory.html'
+    success_url = reverse_lazy('news:index')
 
 class IndexView(generic.ListView):
     template_name = 'news/index.html'
