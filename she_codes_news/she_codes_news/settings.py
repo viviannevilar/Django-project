4398b85586bf80b1d1930fe95d388cc86043102d
viviannevilar <viviannevilar@gmail.com>
2020-11-29 13:20:40 +0800

    settings for deployment

--- she_codes_news/she_codes_news/settings.py
+++ she_codes_news/she_codes_news/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -143,3 +144,7 @@ LOGOUT_REDIRECT_URL = 'news:index'
 LOGIN_URL = 'login'
 
 CRISPY_TEMPLATE_PACK="bootstrap4"
+
+db_from_env = dj_database_url.config(conn_max_age=500) 
+
+DATABASES['default'].update(db_from_env)
